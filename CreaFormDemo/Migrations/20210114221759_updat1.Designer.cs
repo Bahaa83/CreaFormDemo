c861138dba219f26137153d97baa33d25d0e3e08
Bahaa83 <bahaa.love84@gmail.com>
2021-01-14 23:26:05 +0100

    update migration

--- CreaFormDemo/Migrations/20210114221759_updat1.Designer.cs
+++ CreaFormDemo/Migrations/20210114221759_updat1.Designer.cs
@@ -4,14 +4,16 @@ using CreaFormDemo.Entitys;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace CreaFormDemo.Migrations
 {
     [DbContext(typeof(CreaFormDBcontext))]
-    partial class CreaFormDBcontextModelSnapshot : ModelSnapshot
+    [Migration("20210114221759_updat1")]
+    partial class updat1
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ CreaFormDemo/Migrations/20210114221759_updat1.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace CreaFormDemo.Migrations
+{
+    public partial class updat1 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Gatuadressr",
+                table: "Rådgivare",
+                newName: "Gatuadress");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Gatuadress",
+                table: "Rådgivare",
+                newName: "Gatuadressr");
+        }
+    }
+}
