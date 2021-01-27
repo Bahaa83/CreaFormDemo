772c7cdd632f45404634e29222cb67ff37968d4a
Bahaa83 <bahaa.love84@gmail.com>
2021-01-27 15:33:17 +0100

    nhj

--- CreaFormDemo/Services/Repository/ClientRepository.cs
+++ CreaFormDemo/Services/Repository/ClientRepository.cs
@@ -61,10 +61,10 @@ namespace CreaFormDemo.Services.Repository
             }
         }
 
-        public async Task<SymptomsCategory> GetSymptomsQuesbycategory(int orderby)
+        public async Task<IEnumerable<SymptomQuestions>> GetSymptomsQuesbycategory(int orderby)
         {
             var symtomcategori = await db.symptomsCategories.FirstOrDefaultAsync(x => x.OrderBy == orderby);
-             return await db.symptomsCategories.Include(x=>x.symptomQuestions.Where(x=>x.SymptomsCategoryID== symtomcategori.ID).ToArray()).FirstOrDefaultAsync(x=>x.OrderBy==orderby);
+            return await db.symptomQuestions.Include(x => x.symptomsCategory).Where(x => x.SymptomsCategoryID == symtomcategori.ID).ToListAsync();
             //return await db.symptomQuestions.Where(x => x.SymptomsCategoryID == symtomcategori.ID).ToListAsync();
            
         }
