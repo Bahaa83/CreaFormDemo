1d469174f364aa7677bb63b1a4c39d905b15ad48
Bahaa83 <bahaa.love84@gmail.com>
2021-01-27 16:26:40 +0100

    gbnh

--- CreaFormDemo/Startup.cs
+++ CreaFormDemo/Startup.cs
@@ -47,7 +47,11 @@ namespace CreaFormDemo
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddControllers();
+            services.AddControllers().AddNewtonsoftJson(options =>
+            
+options.SerializerSettings.ReferenceLoopHandling=Newtonsoft.Json.ReferenceLoopHandling.Ignore
+            );
+           
             services.AddDbContext<CreaFormDBcontext>
                (options => options.UseSqlServer(Configuration.GetConnectionString("Connst")));
             services.AddScoped<IAuthRepository,AuthRepository>();
