631aafe3b6c266109b0390524390fa6f2df83dbc
Bahaa83 <bahaa.love84@gmail.com>
2021-02-05 19:22:02 +0100

    uhbgt

--- CreaFormDemo/Services/Repository/AdvisorRepository.cs
+++ CreaFormDemo/Services/Repository/AdvisorRepository.cs
@@ -60,36 +60,26 @@ namespace CreaFormDemo.Services.Repository
         {
             var symtomsviews = new List<SymtomOverview>();
             var clientsymtomanswer = await GetClientSymtomAnsewr(clientid);
-            var categorys = await db.symptomsCategories.OrderBy(x=>x.OrderBy).ToListAsync();
+            //var categorys = await db.symptomsCategories.OrderBy(x=>x.OrderBy).ToListAsync();
 
             var Symtomoverview = new SymtomOverview();
 
 
-            //for (int i = 0; i < categorys.Count(); i++)
-            //{
-            foreach (var item in clientsymtomanswer)
+            for (int i = 1; i < 15; i++)
+            {
+                foreach (var item in clientsymtomanswer)
                 {
-              
-
-                if ( item.SymtomCategoryID==4)
+                if ( item.SymtomCategoryID == i)
                     {
-                       
                         Symtomoverview.SymtomCategoryID = item.SymtomCategoryID;
                         Symtomoverview.TotalFrequency += item.Frequency;
                         Symtomoverview.TotalDifficulty += item.Difficulty;
                         Symtomoverview.totalNumberofsymptoms += item.Numberofsymptoms;
-
-                        
                     }
-                  
-                //}
-              
-                //Symtomoverview.ClientID = clientid;
-                //await db.symtomOverviews.AddAsync(Symtomoverview);
-                //await Save();
 
+                }
+                symtomsviews.Add(Symtomoverview);
             }
-            symtomsviews.Add(Symtomoverview);
 
             return symtomsviews;
 
